0e0925b10dd2e5fcb8296f3d61aaff7f6fb6fed0
Edgar dos Reis <echo@DESKTOP-VB6NUK4>
2023-12-28 21:07:06 -0300

    tested

--- examples/server-minimal-simulation.py
+++ examples/server-minimal-simulation.py
@@ -22,25 +22,25 @@ async def main():
     server.set_endpoint("opc.tcp://0.0.0.0:4848/freeopcua/server/")
 
     # set up our own namespace, not really necessary but should as spec
-    uri = "http://examples.freeopcua.github.io" # Nome do Servidor #SenseIoT
+    uri = "SenseIotNamespece" # Nome do Servidor #SenseIotNamespece # http://examples.freeopcua.github.io
     idx = await server.register_namespace(uri)
 
     # populating our address space
     # server.nodes, contains links to very common nodes like objects and root
-    myobj = await server.nodes.objects.add_object(idx, "MyObject")  # Insere um equipamento
-    myvar = await myobj.add_variable(idx, "MyVariable", 6.7)        # Insere as variaveis
-    myvar2 = await myobj.add_variable(idx, "MyVariable2", 0.0)
+    myobj_PLC01 = await server.nodes.objects.add_object(idx, "PLC01")  # Insere um equipamento
+    mytemp01 = await myobj_PLC01.add_variable(idx, "temperatura", 6.7)        # Insere as variaveis
+    mypressure01 = await myobj_PLC01.add_variable(idx, "pressao", 0.0)
     
     # server.nodes, contains links to very common nodes like objects and root
-    myobj2 = await server.nodes.objects.add_object(idx, "MyObject2")  # Insere um equipamento
-    mytemp = await myobj2.add_variable(idx, "temperatura", 6.7)        # Insere as variaveis
-    mypressure = await myobj2.add_variable(idx, "pressao", 0.0)
+    myobj_PLC02 = await server.nodes.objects.add_object(idx, "PLC02")  # Insere um equipamento
+    mytemp02 = await myobj_PLC02.add_variable(idx, "temperatura", 6.7)        # Insere as variaveis
+    mypressure02 = await myobj_PLC02.add_variable(idx, "pressao", 0.0)
 
-    # Set MyVariable to be writable by clients
-    await myvar.set_writable()
-    await myvar2.set_writable()
-    await mytemp.set_writable()
-    await mypressure.set_writable()
+    # Set mytemp0201iable to be writable by clients
+    await mytemp01.set_writable()
+    await mypressure01.set_writable()
+    await mytemp02.set_writable()
+    await mypressure02.set_writable()
     
     await server.nodes.objects.add_method(
         ua.NodeId("ServerMethod", idx),
@@ -62,12 +62,12 @@ async def main():
         while True:
             await asyncio.sleep(1)
             
-            # Gera a forma senoidal aleatória para MyVariable2
+            # Gera a forma senoidal aleatória para mytemp0201iable2
             valor_senoide_aleatoria = senoide_aleatoria(tempo, amplitude=1.0, frequencia=1.0, amplitude_ruido=0.1)
             new_val2 = valor_senoide_aleatoria[len(valor_senoide_aleatoria) - 1]
             
-            _logger.info("Set value of %s to %.1f", myvar, new_val2)
-            await myvar2.write_value(new_val2)
+            _logger.info("Set value of %s to %.1f", mytemp01, new_val2)
+            await mypressure01.write_value(new_val2)
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
